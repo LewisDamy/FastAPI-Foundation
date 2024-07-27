dfae45073d64ef2e56dfa0ae717b576e498b4052
Lewis_Damy <ldamy16@gmail.com>
2024-07-27 12:10:10 -0300

    upgrade: added variables to env

--- TODOApp/routes/auth.py
+++ TODOApp/routes/auth.py
@@ -8,17 +8,18 @@ from passlib.context import CryptContext
 from database import SessionLocal
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
 
-router = APIRouter(
-    prefix='/auth',
-    tags=['auth']
-)
+load_dotenv()
 
-SECRET_KEY = '6b8d8c646fda048f5c03a3a14045eebd'
-ALGORITHM = 'HS256'
+router = APIRouter(prefix="/auth", tags=["auth"])
 
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -55,24 +56,31 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
-def create_access_token(username: str, user_id: int, user_role: str, expires_delta: timedelta):
-    encode = {'sub': username, 'id': user_id, 'role': user_role}
+def create_access_token(
+    username: str, user_id: int, user_role: str, expires_delta: timedelta
+):
+    encode = {"sub": username, "id": user_id, "role": user_role}
     expires = datetime.utcnow() + expires_delta
-    encode.update({'exp': expires})
+    encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: int = payload.get('id')
-        user_role: str = payload.get('role')
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        user_role: str = payload.get("role")
         if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
-        return {'username': username, 'id': user_id, 'user_role': user_role}
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate user",
+            )
+        return {"username": username, "id": user_id, "user_role": user_role}
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -85,16 +93,22 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
         last_name=create_user_request.last_name,
         role=create_user_request.role,
         hashed_password=bcrypt_context.hash(create_user_request.password),
-        is_active=True
+        is_active=True,
     )
     db.add(create_user_model)
     db.commit()
 
 
 @router.post("/token")
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
-    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
-    return {'access_token': token, 'token_type': 'bearer'}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
+    token = create_access_token(
+        user.username, user.id, user.role, timedelta(minutes=20)
+    )
+    return {"access_token": token, "token_type": "bearer"}
